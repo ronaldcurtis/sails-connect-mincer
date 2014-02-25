7c32543fd158d5cf75c6b8a40d41e822d54e6fc0
Ronald Curtis <curtis.ronald@abc.net.au>
2014-02-25 15:39:45 +1100

    Add connect-mincer middleware file

---
+++ config/express.js
@@ -0,0 +1,36 @@
+var connectMincer = require('connect-mincer'),
+		nib = require('nib'),
+    path = require('path'),
+    mincer;
+
+
+// Config for connect-mincer
+// See https://github.com/clarkdave/connect-mincer for more details
+mincer = new connectMincer({
+  root: path.join(__dirname, '../'),
+  production: process.env.NODE_ENV === 'production',
+  mountPoint: '/assets',
+  manifestFile: path.join(__dirname, '../.tmp/public/assets/manifest.json'),
+  paths: ['assets/']
+});
+
+// Configure Stylus so it can import css files just like .styl files
+// Also allows the use of nib (mixin library for stylus)
+mincer.Mincer.StylusEngine.configure(function(style) {
+  style.set('include css', true);
+  style.use(nib());
+});
+
+module.exports = {
+ express: {
+    customMiddleware: function(app){
+      app.use(mincer.assets());
+
+      // Connect-mincer serves our assets in dev
+      // We must precompile our assets before starting in production
+      if (process.env.NODE_ENV !== 'production') {
+        app.use('/assets', mincer.createServer());
+      }
+    }
+  }
+};
