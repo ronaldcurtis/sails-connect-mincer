1d9f69c7a0f1436b3df2d550f3dd95e4cba3ec63
Ronald Curtis <curtis.ronald@abc.net.au>
2014-02-26 10:59:11 +1100

    Add loader script

---
+++ assets/js/foo/anotherexample.coffee
@@ -0,0 +1,1 @@
+foo = 'Connect Mincer Rocks'
---
+++ assets/js/loader.js
@@ -0,0 +1,5 @@
+// See https://github.com/nodeca/mincer For more details about the directives below
+//= require ./socket.io
+//= require ./sails.io
+//= require ./app
+//= require_tree ./foo
