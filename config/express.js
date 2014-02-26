d8761032d28043309b18a6ee9845854fec49b108
Ronald Curtis <curtis.ronald@abc.net.au>
2014-02-27 10:59:25 +1100

    Update middleware comments

--- config/express.js
+++ config/express.js
@@ -15,13 +15,13 @@ mincer = new connectMincer({
 
 // Configure Stylus so it can import css files just like .styl files
 // Also allows the use of nib (mixin library for stylus)
-// See
+// Remove is you're not using nib or stylus
 mincer.Mincer.StylusEngine.configure(function(style) {
   style.set('include css', true);
   style.use(nib());
 });
 
-// Uncomment the lines below to configure Jade or Coffee 
+// Uncomment the lines below to top optionally configure Jade or Coffee 
 // See http://nodeca.github.io/mincer for more details
 
 // mincer.Mincer.JadeEngine.configure({});
